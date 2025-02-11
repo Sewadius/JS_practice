5ee5656ab91d9ff24e655ddb5e28629cf073cf45
Sewadius <vertnather@gmail.com>
2025-02-11 16:21:42 +0300

    added 3.9 & 3.10

---
+++ Stepik/YourJS/3.10 Deepening into Strings/001.js
@@ -0,0 +1,8 @@
+// Углубление в строки
+'use strict';
+
+const num = 25.25;
+
+// Число в строку
+console.log(num.toString());
+console.log(String(num));
---
+++ Stepik/YourJS/3.9 String/001.js
@@ -0,0 +1,17 @@
+// Строки
+'use strict';
+
+const str1 = 'Привет';
+const str2 = "Привет";
+const str3 = `Привет`;
+
+const count = 5;
+console.log(`У меня есть ${count} яблок`);
+
+// Перенос строк
+const str = `
+Привет.
+Пока.
+`;
+
+console.log(str);
